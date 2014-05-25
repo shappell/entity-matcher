dc41adff15c976938138cb261c1891e0f2269bbf
Scott Happell <scott@ispot.tv>
2014-05-25 13:15:57 -0700

    removed very old company header info which no longer applies

--- src/org/shappell/ahocorasick/StringSearchResult.java
+++ src/org/shappell/ahocorasick/StringSearchResult.java
@@ -1,14 +1,3 @@
-/************************************************************************************
- *  $Name:$
- * 
- *  $Id$                                                            
- *  Copyright (C) 2000-2010 MusicNet, Inc. 
- * 
- *  All rights reserved. 
- *  This source code is property of MusicNet, Inc. 
- *  No modifications or additions to the source can be made without written consent from MusicNet, Inc.
- * 
- *************************************************************************************/
 
 package org.shappell.ahocorasick;
 
